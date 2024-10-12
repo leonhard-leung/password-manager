272fb6bd3626dc36a66a2e36ce8610f71c5c5b52
Leonhard Leung <Leonhardleung@gmail.com>
2024-10-12 22:03:31 +0800

    feat: Created the initial structure of the clap module

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(name = "password-manager")]
+#[clap(author = "Leonhard Leung", version, about = "A simple CLI password manager", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add a new account and its password
+    Add {
+        account: String,
+        password: String,
+    },
+    /// Get the password for an account
+    Get {
+        account: String,
+    },
+    /// Display the list of saved accounts
+    List {},
+    /// Generate a random password for an account
+    Gen {
+        length: usize,
+    }
+}
+
 fn main() {
+    let cli = Cli::parse();
 }
