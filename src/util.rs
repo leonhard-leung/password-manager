54d1dcb5173caaad4dd0d70365a52f0c22aa54a4
Leonhard Leung <Leonhardleung@gmail.com>
2024-10-17 21:50:13 +0800

    feat: More secure data location
    This commit changes the directory location of the json file. The default location is in C: which can be problematic since this application stores user-specific data, which is why the current implementation now stores the data in the AppData of the user.

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::{File, OpenOptions};
 use std::fs::create_dir_all;
 use std::io;
@@ -6,26 +6,9 @@ use std::io::{BufReader, BufWriter, Read, Write};
 use serde_json::to_writer_pretty;
 use crate::manager::Account;
 
-const FILE_PATH: &str = "C:\\Password Manager\\data\\passwords.json";
-
-pub fn file_exists() -> bool{
-    let path = Path::new(FILE_PATH);
-
-    if let Some(parent) = path.parent() {
-        if !parent.exists() {
-            create_dir_all(parent).expect("Could not create directories");
-        }
-    }
-
-    if !path.exists() {
-        File::create(path).expect("Could not create file");
-    }
-
-    path.exists()
-}
-
 pub fn get_data() -> serde_json::Result<Vec<Account>> {
-    let file = File::open(FILE_PATH).expect("Could not open file");
+    let path = get_password_file_path();
+    let file = File::open(&path).expect("Could not open file");
     let mut reader = BufReader::new(file);
 
     let mut contents = String::new();
@@ -51,17 +34,19 @@ pub fn save_to_file(account: Account) {
 
     accounts.push(account);
 
+    let path = get_password_file_path();
+
     let file = OpenOptions::new()
         .write(true)
         .truncate(true)
-        .open(FILE_PATH)
-        .expect("Could not create file");
+        .open(path)
+        .expect("Could not save to file");
 
     let writer = BufWriter::new(file);
 
     to_writer_pretty(writer, &accounts).unwrap();
 
-    println!("Wrote {} bytes", accounts.len());
+    println!("Wrote {} {}", accounts.len(), if accounts.len() == 1 { "byte" } else { "bytes" });
     println!("Success: The entry for '{}' has been saved successfully", label);
 }
 
@@ -72,4 +57,26 @@ pub fn get_user_input(message: &str) -> String {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     input.trim().to_string()
+}
+
+pub fn get_password_file_path() -> PathBuf {
+    let app_data_path = std::env::var("APPDATA").unwrap();
+    let path = Path::new(&app_data_path).join("Password Manager/data/passwords.json");
+
+    // Check if the parent directory (up to "Password Manager") exists
+    if let Some(parent) = path.parent().unwrap().parent().unwrap() {
+        create_dir_all(parent).expect("Could not create directory structure");
+    }
+
+    // Create the passwords.json file if it doesn't exist
+    if !path.exists() {
+        File::create(&path).expect("Could not create file");
+    }
+
+    path
+}
+
+pub fn file_exists() -> bool{
+    let path = get_password_file_path();
+    path.exists()
 }
